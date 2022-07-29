264dc37f0a6028b1bcdb2c28047ec031c00696f9
Dev Prumo <dev@prumoengenharia.com.br>
2022-07-29 13:05:28 -0300

    config server.js

--- server.js
+++ server.js
@@ -13,3 +13,7 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.post('/', function(req, res) {
+    res.redirect('/');
+});
