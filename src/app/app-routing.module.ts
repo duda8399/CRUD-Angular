6d2435a61da5002ad23e066d2cdb1bd6173b4da9
Dev Prumo <dev@prumoengenharia.com.br>
2022-07-29 13:00:11 -0300

    correção rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  //{path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: '', loadChildren: () => import('./modules/companies/companies.module').then(m => m.CompaniesModule)},
 ];
 
